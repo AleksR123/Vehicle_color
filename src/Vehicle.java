181f828288ef656253cc2b2ab9c443125ad8d9da
unknown <aleksandarristov28@gmail.com>
2023-12-23 00:25:54 +0100

    first java program

---
+++ src/Vehicle.java
@@ -0,0 +1,22 @@
+
+public class Vehicle {
+
+	private String color; 
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+		
+	}
+	private int x;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+		
+	}
+
+	}
---
+++ src/color.java
@@ -0,0 +1,18 @@
+
+public class color {
+	
+
+	public static void main(String[] args) {
+		
+		Vehicle v1 = new Vehicle();
+		v1.setColor("Red");
+		System.out.println(v1.getColor());
+		v1.setX(10);
+		System.out.println(v1.getX());
+		
+	}
+
+}
+
+
+
